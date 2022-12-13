4d372b6315ea862a1377ceacaaaed4f1c184588c
EC2 Default User <ec2-user@ip-172-31-81-112.ec2.internal>
2022-12-13 00:58:00 +0000

    computed basic statistics for experiments

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -13,14 +13,14 @@ if __name__ == "__main__":
     if os.path.exists(pred_path) is False:
         os.makedirs(pred_path)
         
-    mnist_train_maps = np.load("./MNIST-train/fc1-maps.npy")
-    # mnist_train_maps = np.load("./MNIST-train/conv2-flattened-maps.npy")
+    # mnist_train_maps = np.load("./MNIST-train/fc1-maps.npy")
+    mnist_train_maps = np.load("./MNIST-train/conv2-flattened-maps.npy")
 
-    mnist_test_maps = np.load("./MNIST-test/fc1-maps.npy")
-    # mnist_test_maps = np.load("./MNIST-test/conv2-flattened-maps.npy")
+    # mnist_test_maps = np.load("./MNIST-test/fc1-maps.npy")
+    mnist_test_maps = np.load("./MNIST-test/conv2-flattened-maps.npy")
     
-    notmnist_maps = np.load("./notMNIST/fc1-maps.npy")
-    # notmnist_maps = np.load("./notMNIST/conv2-flattened-maps.npy")
+    # notmnist_maps = np.load("./notMNIST/fc1-maps.npy")
+    notmnist_maps = np.load("./notMNIST/conv2-flattened-maps.npy")
 
     # combined_maps = np.load("./MNIST-notMNIST-combined/fc1-maps.npz.npy")
 
@@ -28,12 +28,12 @@ if __name__ == "__main__":
     clf.fit(mnist_train_maps)
 
     predictions = clf.predict(mnist_test_maps)
-    np.save(f"./{pred_path}/mnist-test-fc1", predictions)
+    np.save(f"./{pred_path}/mnist-test-conv2", predictions)
     anomaly_scores = clf.score_samples(mnist_test_maps)
-    np.save(f"./{scores_path}/mnist-test-fc1", anomaly_scores)
+    np.save(f"./{scores_path}/mnist-test-conv2", anomaly_scores)
 
     predictions = clf.predict(notmnist_maps)
-    np.save(f"./{pred_path}/notmnist-fc1", predictions)
+    np.save(f"./{pred_path}/notmnist-conv2", predictions)
     anomaly_scores = clf.score_samples(notmnist_maps)
-    np.save(f"./{scores_path}/notmnist-fc1", anomaly_scores)
+    np.save(f"./{scores_path}/notmnist-conv2", anomaly_scores)
     
